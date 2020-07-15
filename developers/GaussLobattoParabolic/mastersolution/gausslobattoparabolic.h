80bbccc3f926074825b816ca7d46bc380f32ca5e
ralfh <ralfh@ethz.ch>
2020-07-15 19:26:40 +0200

    Editing of homework problems

---
+++ developers/GaussLobattoParabolic/mastersolution/gausslobattoparabolic.h
@@ -0,0 +1,17 @@
+/**
+ * @ file
+ * @ brief NPDE homework TEMPLATE HEADER FILE
+ * @ author
+ * @ date
+ * @ copyright Developed at SAM, ETH Zurich
+ */
+
+#ifndef GLP_H_
+#define GLP_H_
+
+#include <Eigen/Core>
+#include <iostream>
+
+namespace GLP {}
+
+#endif
