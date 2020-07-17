77c20ccc080d0c7e8cc674739a31dbbf5e53587d
ralfh <ralfh@ethz.ch>
2020-07-17 17:50:42 +0200

    Editing of homework problems

---
+++ developers/ConsLawWithSource/mastersolution/conslawwithsource.h
@@ -0,0 +1,17 @@
+/**
+ * @ file
+ * @ brief NPDE homework TEMPLATE HEADER FILE
+ * @ author
+ * @ date
+ * @ copyright Developed at SAM, ETH Zurich
+ */
+
+#ifndef CLWS_H_
+#define CLWS_H_
+
+#include <Eigen/Core>
+#include <iostream>
+
+namespace CLWS {}
+
+#endif
